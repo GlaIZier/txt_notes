04951f70710379b17e38785b3fac27e1c1ff581c
Mikhail Khokhlushin <mkhokhlushin@smile-net.ru>
2016-11-14 13:55:47 +0300

    Daily autocommit

--- daily_autocommit_script.sh
+++ daily_autocommit_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo -----------------------------------------------------------------------------------
 echo $(date)
+cd /home/mkhokhlushin/github/txt_notes
 git add -A
 git commit -m "Daily autocommit"
 exit 0 
